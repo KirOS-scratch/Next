042e509f0ea2d9566122facfffd34801840b047a
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-01 19:37:08 +0300

    Update page.js

--- js/wiki/ru/page.js
+++ js/wiki/ru/page.js
@@ -2,4 +2,15 @@ let { body, head } = document
 const url = new URL(window.location)
 const sP = url.searchParams
 const page = sp.get("p")
-if (page === "main") {}
+if (page === "main") {
+  head.innerHTML = `
+    <title>Главная страница KirOS Next Вики</title>
+  `
+  body.innerHTML = `
+    <h1>Вики про KirOS Next</h1>
+    <h2>Ссылки</h2>
+    <p><a href="?p=Download"><button>Download</button></a></p>
+    <p><a href="?p=OpenInBrowser"><button>Open in browser</button></a></p>
+    <p><a href="?p=documentation"><button>Documentation</button></a></p>
+  `
+}
